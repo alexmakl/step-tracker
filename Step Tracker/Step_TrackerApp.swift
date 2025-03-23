9dec6ea3f65dec215c0054561712479b95b1719a
Alexander Maklakov <richmak@ya.ru>
2025-03-23 22:26:08 +0300

    xcode project created

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Alexander on 23.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
