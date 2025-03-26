611bc487fa4f5bedd5db6508e91e82f74da96402
Alexander <mclakov@yandex.ru>
2025-03-26 10:02:37 +0300

    weight line chart basic ui

--- Step Tracker/Charts/StepPieChart.swift
+++ Step Tracker/Charts/StepPieChart.swift
@@ -74,5 +74,5 @@ struct StepPieChart: View {
 }
 
 #Preview {
-    StepPieChart(chartData: ChartMath.averageWeekdayCount(for: HealthMetric.mockData))
+    StepPieChart(chartData: ChartMath.averageWeekdayCount(for: MockData.steps))
 }
