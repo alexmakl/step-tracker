3323e43406bcfaca2c748e273520d967f596395c
Alexander <mclakov@yandex.ru>
2025-03-29 18:06:13 +0300

    delete adding mock data to simulator

--- Step Tracker/Managers/HealthKitManager.swift
+++ Step Tracker/Managers/HealthKitManager.swift
@@ -136,26 +136,4 @@ class HealthKitManager {
         let startDate = calendar.date(byAdding: .day, value: -daysBack, to: endDate)!
         return DateInterval(start: startDate, end: endDate)
     }
-    
-//    func addSimulatorData() async {
-//        var sampleObjects: [HKQuantitySample] = []
-//        
-//        for i in 0..<28 {
-//            let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 4_000...20_000))
-//            let weightQuantity = HKQuantity(unit: .pound(), doubleValue: .random(in: (160 + Double(i/3)...165 + Double(i/3))))
-//            
-//            let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
-//            let endDate = Calendar.current.date(byAdding: .day, value: -i+1, to: .now)!
-//            
-//            let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepQuantity, start: startDate, end: endDate)
-//            let weightSample = HKQuantitySample(type: HKQuantityType(.bodyMass), quantity: weightQuantity, start: startDate, end: endDate)
-//            
-//            sampleObjects.append(stepSample)
-//            sampleObjects.append(weightSample)
-//        }
-//        
-//        
-//        try! await store.save(sampleObjects)
-//        print("✅ Mock Data sent up")
-//    }
 }
